463ebf4484d060d83e0c403675a0e9174c66f44c
Severin <sevefriede@gmail.com>
2016-12-01 22:02:44 +0100

    Updated example and removed some lint errors

--- src/main.tsx
+++ src/main.tsx
@@ -6,39 +6,73 @@ import "material-design-lite";
 import "material-design-lite/dist/material.indigo-pink.min.css";
 
 import { Component, h, render } from "preact";
-import { Layout, Navigation, Card, Button, Icon, TextField } from "preact-mdl";
+import { Button, Card, Icon, Layout, Navigation, TextField } from "preact-mdl";
 import { Router } from "preact-router";
+
 import "./main.css";
 
 const React = { createElement: h };
 
 // Our top-level component.
 class App extends Component<{}, {}> {
-    handleFab = () => {
-        alert('You clicked New!');
-    };
 
-    render() {
+    public render() {
         return (
             <div id="app">
-                <Layout fixed-header fixed-drawer>
-                    <Header onSearch={() => { return; } } />
-                    <Sidebar />
-
-                    <Button id="fab" fab colored onClick={this.handleFab}>
-                        <Icon icon="create" />
-                    </Button>
-
-                    <Layout.Content>
-                        <Router>
-                            <Home path="/" default />
-                            <Profile path="/profile" id="me" />
-                            <Profile path="/profile/:id" id="" />
-                        </Router>
-                    </Layout.Content>
-                </Layout>
+                <SideLayout />
             </div>
         );
+    }
+}
+
+class MaterialLayoutHelper {
+    constructor(private layout: Layout) { }
+
+    public toggleDrawer(): void {
+        this.layout.base.MaterialLayout.toggleDrawer();
+    }
+
+    public get isSmallScreen(): boolean {
+        return this.layout.base.classList.contains('is-small-screen');
+    }
+
+    public get hasFixedDrawer(): boolean {
+        return this.layout.base.classList.contains('mdl-layout--fixed-drawer');
+    }
+}
+
+class SideLayout extends Component<{}, {}> {
+
+    public render() {
+        return (
+            <Layout fixed-header fixed-drawer>
+                <Header onSearch={() => { return; } } />
+                <Sidebar onDrawerClick={this.toggleDrawer} />
+
+                <Button id="fab" fab colored onClick={this.handleFab}>
+                    <Icon icon="create" />
+                </Button>
+
+                <Layout.Content>
+                    <Router>
+                        <Home path="/" default />
+                        <Profile path="/profile" id="me" />
+                        <Profile path="/profile/:id" id="" />
+                    </Router>
+                </Layout.Content>
+            </Layout>);
+    }
+
+    private toggleDrawer = () => {
+        let layout = new MaterialLayoutHelper(this);
+        if (layout.hasFixedDrawer && !layout.isSmallScreen) {
+            return;
+        }
+        layout.toggleDrawer();
+    }
+
+    private handleFab = () => {
+        alert('You clicked New!');
     }
 }
 
@@ -54,24 +88,19 @@ const Header = ({ onSearch }) => (
                 type="search"
                 onSearch={onSearch}
                 style="background-color:#FFF; color:#000; padding:10px;"
-            />
+                />
         </Layout.HeaderRow>
     </Layout.Header>
 );
 
-
-class Sidebar extends Component<{}, {}> {
-    shouldComponentUpdate() {
+class Sidebar extends Component<{ onDrawerClick }, {}> {
+    public shouldComponentUpdate() {
         return false;
     }
 
-    hide = () => {
-        this.base.classList.remove('is-visible');
-    };
-
-    render() {
+    public render({ onDrawerClick }) {
         return (
-            <Layout.Drawer onClick={this.hide}>
+            <Layout.Drawer onClick={onDrawerClick}>
                 <Layout.Title>Example App</Layout.Title>
                 <Navigation>
                     <Navigation.Link href="/">Home</Navigation.Link>
@@ -83,17 +112,17 @@ class Sidebar extends Component<{}, {}> {
     }
 }
 
-interface RouterProps extends JSX.HTMLAttributes {
+interface IRouterProps extends JSX.HTMLAttributes {
     default?: boolean;
     path: string;
 }
 
-class Home extends Component<RouterProps, {}> {
-    shouldComponentUpdate() {
+class Home extends Component<IRouterProps, {}> {
+    public shouldComponentUpdate() {
         return false;
     }
 
-    render() {
+    public render() {
         return (
             <Card shadow={4}>
                 <Card.Title class="graphic">
@@ -111,12 +140,12 @@ class Home extends Component<RouterProps, {}> {
     }
 }
 
-class Profile extends Component<{id} & RouterProps, {}> {
-    shouldComponentUpdate({id}) {
+class Profile extends Component<{ id } & IRouterProps, {}> {
+    public shouldComponentUpdate({id}) {
         return id !== this.props.id;
     }
 
-    render({id}) {
+    public render({id}) {
         return (
             <div class="profile">
                 <Card shadow={3} class="wide">
