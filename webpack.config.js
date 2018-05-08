e780eea0f1ef8a719e7e95b778f0b0fac4a56cbe
Trevor Bekolay <tbekolay@gmail.com>
2018-05-07 21:50:44 -0400

    Update requirements
    And update webpack config to new API.

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,49 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    context: __dirname,
-    entry: './src/main.tsx',
-    output: {
-        path: './dist',
-        filename: 'main.js',
-    },
-    resolve: {
-        extensions: ['', '.js', '.ts', '.tsx']
-    },
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' },
-            { test: /\.ico$/, loader: 'file-loader?name=[name].[ext]' },
-            { test: /\.(png|jpg|gif)$/, loader: 'url-loader?limit=8192' },
-            {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'url-loader?limit=8192&mimetype=application/font-woff&name=./[hash].[ext]'
-            },
-            {
-                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'file-loader?name=./[hash].[ext]'
-            },
-            { test: /\.tsx?$/, loader: 'ts-loader' },
-        ],
-    },
-    plugins: [
-        require('webpack-fail-plugin'),
-        new HtmlWebpackPlugin({title: "preact-mdl example"})
-    ],
-    postcss: function(webpack) {
-        return [
-            require('postcss-nested'),
-            require('autoprefixer'),
-        ];
-    }
-}
+  context: __dirname,
+  entry: "./src/main.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: loader => [
+                require("postcss-nested"),
+                require("autoprefixer")
+              ]
+            }
+          }
+        ]
+      },
+      { test: /\.ico$/, use: "file-loader?name=[name].[ext]" },
+      { test: /\.(png|jpg|gif)$/, use: "url-loader?limit=8192" },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use:
+          "url-loader?limit=8192&mimetype=application/font-woff&name=./[name].[ext]"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: "file-loader?name=./[hash].[ext]"
+      },
+      { test: /\.tsx?$/, use: "ts-loader" }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ title: "preact-mdl example" })],
+  mode: 'none'
+};
